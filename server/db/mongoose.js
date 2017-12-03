f2710a5298751887dfcf328f3d4708de463c03f5
Robert Rivest <robertrivest68@gmail.com>
2017-12-03 10:09:28 -0500

    Redeploy Heroku

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -4,5 +4,4 @@ const mongoose = require('mongoose');
 mongoose.Promise = global.Promise;
 //connect using mongoose
 mongoose.connect(process.env.MONGODB_URI ||  'mongodb://localhost:27017/TodoApp');
-
 module.exports =  {mongoose};
